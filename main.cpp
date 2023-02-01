2e5000d5ae02c148404222d543b29304ad1b7bdc
Shakall1944 <lukas.kalan@gmail.com>
2023-02-01 22:27:59 +0100

    Initial setup

--- main.cpp
+++ main.cpp
@@ -8,10 +8,27 @@ int main()
     windowDimensions[0] = 384;
     windowDimensions[1] = 384;
     InitWindow(windowDimensions[0], windowDimensions[1], "Classy clash game!");
+    
+    //Load map from texture
+    Texture2D worldmap = LoadTexture("nature_tileset/WorldMap32x32.png");
+    float worldPosX{};
+    float worldPosY{};
 
-    // show map on the window
-    Texture2D worldmap = LoadTexture("nature_tileset/WorldMap.png");
-    Vector2 worldPos{0,0};
-    DrawTextureEx(worldmap, worldPos, 0.0, 4, WHITE);
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        //Start drawing
+        BeginDrawing();
+        ClearBackground(WHITE);
+        
+
+        Vector2 worldPos{worldPosX - worldmap.width/2, worldPosY - worldmap.height/2};
+        // draw map on the window
+        DrawTextureEx(worldmap, worldPos, 0.0, 4, WHITE);
+
+        EndDrawing();
+    }
+    UnloadTexture(worldmap);
+    CloseWindow();
 
 }
