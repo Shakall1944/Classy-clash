509d62bb63d56ec9622f0ef80b87f2360c8863ab
Shakall1944 <lukas.kalan@gmail.com>
2023-02-02 21:59:59 +0100

    Movement and draw of char

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main()
 {
@@ -13,6 +14,15 @@ int main()
     Texture2D worldmap = LoadTexture("nature_tileset/WorldMap32x32.png");
     float worldPosX{};
     float worldPosY{};
+    Vector2 worldPos{worldPosX - worldmap.width/2, worldPosY - worldmap.height/2};
+    float speed{4.0};
+    
+    //load character
+    Texture2D knigth = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knigthPos{
+        (float)windowDimensions[0]/2.0f - 4.0f * (0.5f * (float)knigth.width/6.0f),
+        (float)windowDimensions[1]/2.0f - 4.0f * (0.5f * (float)knigth.height)
+    };
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -21,10 +31,36 @@ int main()
         BeginDrawing();
         ClearBackground(WHITE);
         
+        Vector2 direction{};
 
-        Vector2 worldPos{worldPosX - worldmap.width/2, worldPosY - worldmap.height/2};
+        
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+        if (Vector2Length(direction) != 0.0)
+        {
+            //set worldPos = worldPos - direction + scale vector
+            
+            worldPos = Vector2Subtract(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        }
+
         // draw map on the window
         DrawTextureEx(worldmap, worldPos, 0.0, 4, WHITE);
+        // draw the character
+        Rectangle source;
+        source.width = (float)knigth.width/6.f;
+        source.height = (float)knigth.height;
+        source.x = 0.0;
+        source.y = 0.0;
+
+        Rectangle dest;
+        dest.width = 4.0f * (float)knigth.width/6;
+        dest.height = 4.0f * (float)knigth.height;
+        dest.x = knigthPos.x;
+        dest.y = knigthPos.y;
+        Vector2 origin{};
+        DrawTexturePro(knigth, source, dest, origin, 0.f, WHITE);
 
         EndDrawing();
     }
