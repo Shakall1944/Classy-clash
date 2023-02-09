2c55708c6998acdc8d77ec9a2bdf712ac89e8e18
Shakall1944 <lukas.kalan@gmail.com>
2023-02-09 21:10:22 +0100

    Attack!

--- Character.h
+++ Character.h
@@ -8,17 +8,18 @@
 
 class Character : public BaseCharacter
 {
-private:
-    int windowWidth{};
-    int windowHeight{};
-    Texture2D weapon{LoadTexture("characters/weapon_sword.png")};
-    Rectangle weaponCollisionRec{};
-
 public:
     Character(int winWith, int winHeigth);
     //void setScreenPos(int windWidth, int winHeight);
     virtual void tick(float deltaTime) override;
     virtual Vector2 getScreenPos() override;
-};
+    Rectangle getWeaponCollisonRec() { return weaponCollisionRec; }
 
+
+private:
+    int windowWidth{};
+    int windowHeight{};
+    Texture2D weapon{LoadTexture("characters/weapon_sword.png")};
+    Rectangle weaponCollisionRec{};
+};
 #endif
