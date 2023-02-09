3cd927f87527907fc1c0772d2bfe6df2bdc80d4f
Shakall1944 <lukas.kalan@gmail.com>
2023-02-09 20:49:19 +0100

    drawing sword

--- Character.cpp
+++ Character.cpp
@@ -1,13 +1,12 @@
 #include "Character.h"
 #include "raymath.h"
 
-Character::Character(int windWidth, int winHeigth) :
-    windowWidth(windWidth),
-    windowHeight(winHeigth)
-{   
+Character::Character(int windWidth, int winHeigth) : 
+        windowWidth(windWidth),
+        windowHeight(winHeigth)
+{
     width = texture.width / maxFrames;
     heigth = texture.height;
-  
 }
 
 /*void Character::setScreenPos(int windWidth, int winHeight)
@@ -21,16 +20,14 @@ Vector2 Character::getScreenPos()
 {
     return Vector2{
         static_cast<float>(windowWidth) / 2.0f - scale * (0.5f * width),
-        static_cast<float>(windowHeight) / 2.0f - scale * (0.5f * heigth)
-    };
-
+        static_cast<float>(windowHeight) / 2.0f - scale * (0.5f * heigth)};
 }
 
 void Character::tick(float deltaTime)
 {
-    
+
     // set the direction variable for the movement
-    
+
     // set the movement to all directions
     if (IsKeyDown(KEY_A))
         velocity.x -= 1.0;
@@ -40,10 +37,47 @@ void Character::tick(float deltaTime)
         velocity.y -= 1.0;
     if (IsKeyDown(KEY_S))
         velocity.y += 1.0;
-    
+
     BaseCharacter::tick(deltaTime);
 
+    Vector2 origin{};
+    Vector2 offset{};
+    float rotation{};
+    if (rightLeft > 0.f)
+    {
+        origin = {0.f, weapon.height * scale};
+        offset = {35.f, 55.f};
+        weaponCollisionRec = {
+            getScreenPos().x + offset.x,
+            getScreenPos().y + offset.y - weapon.height * scale,
+            weapon.width * scale,
+            weapon.height * scale
+            };
+        rotation = 35.f;
+    }
+    else
+    {
+        origin = {weapon.width * scale, weapon.height * scale};
+        offset = {25.f, 55.f};
+        weaponCollisionRec = {
+            getScreenPos().x + offset.x - weapon.width * scale,
+            getScreenPos().y + offset.y - weapon.height * scale,
+            weapon.width * scale,
+            weapon.height * scale
+            };
+        rotation = -35.f;
+    }
 
+    // draw sword
+    Rectangle source{0.f, 0.f, static_cast<float>(weapon.width) * rightLeft, static_cast<float>(weapon.height)};
+    Rectangle dest{getScreenPos().x + offset.x, getScreenPos().y + offset.y, weapon.width * scale, weapon.height * scale};
+    DrawTexturePro(weapon, source, dest, origin, rotation, WHITE);
+
+    DrawRectangleLines(
+        weaponCollisionRec.x,
+        weaponCollisionRec.y,
+        weaponCollisionRec.width,
+        weaponCollisionRec.height,
+        RED
+        );
 }
-
-
